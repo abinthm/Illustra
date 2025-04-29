cf30fd698392c60a7a70d7e8e23cb7d5ba52b212
Abin thomas Jinu <118169847+abinthm@users.noreply.github.com>
2025-04-29 07:37:46 +0530

    Update api.ts

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 
 // API configuration
 export const API_CONFIG = {
-  baseURL: import.meta.env.VITE_API_URL || "https://3c7a-116-68-103-222.ngrok-free.app/",
+  baseURL: import.meta.env.VITE_API_URL || "https://c3c5-116-68-103-222.ngrok-free.app",
 };
